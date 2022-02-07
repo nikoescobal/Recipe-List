bac30e452f586e74ca9001a93d58492a295d38b3
Niko Escobal <niko.escobal@gmail.com>
2022-02-07 21:14:46 +0800

    Remove unnecessary files, reconfigure routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import { Route, Switch } from 'react-router-dom';
-import MainPage from './pages/Main.page';
-
 function App() {
-  return (
-    <Switch>
-      <Route exact path="/" component={MainPage} />
-    </Switch>
-  );
+  return <h1 className="bg-blue-400 text-2xl">Poopy</h1>;
 }
 
 export default App;
